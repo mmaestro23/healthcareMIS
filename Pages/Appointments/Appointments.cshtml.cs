9a1756b224925cd6e0b34aeeca6c46b6ecd39798
maestro <ericmanzi1738@gmail.com>
2023-05-17 20:07:32 +0200

    appointment

--- Pages/Appointments/Appointments.cshtml.cs
+++ Pages/Appointments/Appointments.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,20 +6,129 @@ namespace healthcareMIS.Pages.Appointments
 {
     public class AppointmentsModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		private const string V = "";
+		public AppointmentInfo appointmentInfo = new AppointmentInfo();
+		public List<Patients> patient = new List<Patients>();
+		public List<Doctor> doctor = new List<Doctor>();
+		public String errorMessage = "";
+		public String successMessage = "";
+		public IActionResult OnGet()
+		{
+			String connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=healthcareMIS;Integrated Security=True";
+			using (SqlConnection con = new SqlConnection(connectionString))
+			{
+				con.Open();
+				String sqlQuery = "SELECT * FROM patients";
+				using (SqlCommand cmd = new SqlCommand(sqlQuery, con))
+				{
+					using (SqlDataReader reader = cmd.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							Patients patients = new Patients();
+							patients.id = reader.GetInt32(0);
+							patients.name = reader.GetString(1);
+							patient.Add(patients);
+						}
+					}
+				}
+				String sqlQueryy = "SELECT * FROM doctor";
+				using (SqlCommand cmd = new SqlCommand(sqlQueryy, con))
+				{
+					using (SqlDataReader reader = cmd.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							Doctor doctors = new Doctor();
+							doctors.doctor_id = reader.GetInt32(0); 
+							doctors.firstname = reader.GetString(1);
+							doctors.lastname = reader.GetString(2);
+							doctor.Add(doctors);
+						}
+					}
+				}
+			}
+			return Page();
+		}
+		public void OnPost()
+		{
+			appointmentInfo.patient_id = Convert.ToInt32(Request.Form["patient_id"]);
+			appointmentInfo.doctor_id = Convert.ToInt32(Request.Form["doctor_id"]);
+			appointmentInfo.appointment_date = Convert.ToDateTime(Request.Form["appointment_date"]);
+			appointmentInfo.appointment_time = Request.Form["appointment_time"];
+			appointmentInfo.appointment_type = Request.Form["appointment_type"];
+			appointmentInfo.notes = Request.Form["notes"];
 
-	public class DoctorInfo
+
+			/*            if (vaccinationRecord.CitizenId == 0 || vaccinationRecord.Vaccine.Length == 0)
+						{
+							errorMessage = "All fields are required";
+							return;
+						}*/
+
+			try
+			{
+				String connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=healthcareMIS;Integrated Security=True";
+				using (SqlConnection con = new SqlConnection(connectionString))
+				{
+					con.Open();
+					String sqlQuery = "INSERT INTO appointments(patient_id, doctor_id, appointment_date, appointment_time, appointment_type, notes) VALUES(@patient_id,@doctor_id,@appointment_date,@appointment_time,@appointment_type,@notes)";
+					using (SqlCommand cmd = new SqlCommand(sqlQuery, con))
+					{
+						cmd.Parameters.AddWithValue("@patient_id", appointmentInfo.patient_id);
+						cmd.Parameters.AddWithValue("@doctor_id", appointmentInfo.doctor_id);
+						cmd.Parameters.AddWithValue("@appointment_date", appointmentInfo.appointment_date);
+						cmd.Parameters.AddWithValue("@appointment_time", appointmentInfo.appointment_time);
+						cmd.Parameters.AddWithValue("@appointment_type", appointmentInfo.appointment_type);
+						cmd.Parameters.AddWithValue("@notes", appointmentInfo.notes);
+
+						cmd.ExecuteNonQuery();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				errorMessage = ex.Message;
+				return;
+			}
+			appointmentInfo.appointment_date = null;
+			appointmentInfo.appointment_time = "";
+			appointmentInfo.appointment_type = "";
+			appointmentInfo.notes = "";
+			Response.Redirect("/Appointments/Appointments");
+		}
+	}
+
+	public class Patients
+	{
+		public int id { get; set; }
+		public String name { get; set; }
+		public String email { get; set; }
+		public String phone { get; set; }
+		public String address { get; set; }
+		public DateTime createdAt { get; set; }
+	}
+
+	public class Doctor
+	{
+		public int doctor_id { get; set; }
+		public String firstname { get; set; }
+		public String lastname { get; set; }
+		public String specialization { get; set; }
+		public String email { get; set; }
+		public String phone { get; set; }
+		public String address { get; set; }
+	}
+
+	public class AppointmentInfo
 	{
 		public int appointment_id { get; set; }
-		public String appointment_date { get; set; }
-		public String appointement_time { get; set; }
+		public DateTime? appointment_date { get; set; }
+		public String appointment_time { get; set; }
 		public String appointment_type{ get; set; }
 		public String notes { get; set; }
-		public String patient_id { get; set; }
-		public String doctor_id { get; set; }
+		public int patient_id { get; set; }
+		public int doctor_id { get; set; }
 	}
 
 }
