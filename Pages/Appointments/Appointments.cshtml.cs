b5af932136ec0a05058416e8da19b8c4438c322c
Adede <Adede@DESKTOP-26CN3O2>
2023-05-17 17:43:48 +0300

    adeli

---
+++ Pages/Appointments/Appointments.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace healthcareMIS.Pages.Appointments
+{
+    public class AppointmentsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
