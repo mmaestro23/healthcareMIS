6f15e6ce30cbfccd5ada81c73405024d44f9bad6
maestro <ericmanzi1738@gmail.com>
2023-05-17 15:48:51 +0200

    new one

---
+++ Pages/Doctor/Doctor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace healthcareMIS.Pages.Doctor
+{
+    public class DoctorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
