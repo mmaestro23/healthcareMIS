09981f0011ab9f910f027bd66907162ce63655a6
Adede <Adede@DESKTOP-26CN3O2>
2023-05-17 18:10:31 +0300

    new3

--- Pages/Appointments/Appointments.cshtml.cs
+++ Pages/Appointments/Appointments.cshtml.cs
@@ -9,4 +9,16 @@ namespace healthcareMIS.Pages.Appointments
         {
         }
     }
+
+	public class DoctorInfo
+	{
+		public int appointment_id { get; set; }
+		public String appointment_date { get; set; }
+		public String appointement_time { get; set; }
+		public String appointment_type{ get; set; }
+		public String notes { get; set; }
+		public String patient_id { get; set; }
+		public String doctor_id { get; set; }
+	}
+
 }
