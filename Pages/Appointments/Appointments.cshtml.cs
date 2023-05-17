c633f82f82611a702d51a335308de4f1969cd4c4
maestro <ericmanzi1738@gmail.com>
2023-05-17 17:33:37 +0200

    manz

--- Pages/Appointments/Appointments.cshtml.cs
+++ Pages/Appointments/Appointments.cshtml.cs
@@ -10,7 +10,7 @@ namespace healthcareMIS.Pages.Appointments
         }
     }
 
-	public class DoctorInfo
+	public class AppointmentInfo
 	{
 		public int appointment_id { get; set; }
 		public String appointment_date { get; set; }
@@ -19,6 +19,17 @@ namespace healthcareMIS.Pages.Appointments
 		public String notes { get; set; }
 		public String patient_id { get; set; }
 		public String doctor_id { get; set; }
+	}
+
+	public class DoctorInfo
+	{
+		public int doctor_id { get; set; }
+		public String firstname { get; set; }
+		public String lastname { get; set; }
+		public String specialization { get; set; }
+		public String email { get; set; }
+		public String phone { get; set; }
+		public String address { get; set; }
 	}
 
 }
